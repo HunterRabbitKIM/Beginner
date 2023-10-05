d7f3b1a67c1c085440d0f06a6fb6b5e442e45668
jycc0405 <121859759+jycc0405@users.noreply.github.com>
2023-10-05 20:38:16 +0900

    23-10-05-2
    열쇠 상호작용 추가

--- Assets/Scripts/DoorMove.cs
+++ Assets/Scripts/DoorMove.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,9 @@ using UnityEngine;
 public class DoorMove : MonoBehaviour
 {
     public bool open = false;
-    public float doorOpenAngle = 90f; //¹® ¿­¸± ¶§ °¢µµ
-    public float doorCloseAngle = 0f; //¹® ´ÝÈú ¶§ °¢µµ
-    public float smoot = 2f; //¹®ÀÌ ¿­°í ´ÝÀ»¶§ ºÎµå·¯¿î Á¤µµ
+    public float doorOpenAngle = 90f; //ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+    public float doorCloseAngle = 0f; //ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
+    public float smoot = 2f; //ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½ï¿½ï¿½ ï¿½Îµå·¯ï¿½ï¿½ ï¿½ï¿½ï¿½ï¿½
 
     void Start()
     {
@@ -21,6 +22,11 @@ public class DoorMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        door();
+    }
+
+    public void door()
+    {
         if(open)
         {
             Quaternion targetRotation = Quaternion.Euler(0, doorOpenAngle, 0);
@@ -30,6 +36,19 @@ public class DoorMove : MonoBehaviour
         {
             Quaternion targetRotation2 = Quaternion.Euler(0, doorCloseAngle, 0);
             transform.localRotation = Quaternion.Slerp(transform.localRotation, targetRotation2, smoot * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        IHandle item = other.GetComponent<IHandle>();
+        if (item!=null&&!item.myName().Equals(""))
+        {
+            if (item.myName().Equals("key"))
+            {
+                ChangeDoorState();
+                item.destroy();
+            }
         }
     }
 }
