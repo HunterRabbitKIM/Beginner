58207e2426e9646803d985494c7faab340971e8c
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-10-05 11:53:06 +0900

    Title Scenes & UI Create
    Title Scenes & UI Create

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            if(instance != null)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    public void StartBtn()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    public void Option()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+    public void Sound()
+    {
+
+    }
+
+}
