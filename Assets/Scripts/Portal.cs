f030f569ed39c018c8a01057079ff231ae9c4f57
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-10-13 09:32:22 +0900

    chang2
    chang2

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string sceneToLoad;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if(other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 }
