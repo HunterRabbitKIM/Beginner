6f113fdeab9f6fcde41f115714bfb5ce1f48db66
jycc0405 <121859759+jycc0405@users.noreply.github.com>
2023-11-13 20:37:04 +0900

    11-13

---
+++ Assets/Scripts/Event3.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Event3 : MonoBehaviour
+{
+    public GameObject horrorpic;
+    AudioSource sound;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sound = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Player")
+        {
+            horrorpic.gameObject.SetActive(true);
+            horrorpic.GetComponent<AudioSource>().Play();
+        }
+    }
+}
