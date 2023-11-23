293f281c1de68256c5a2c142b472bfe295a3cb8c
Kim Jeoung Gi <80248899+HunterRabbitKIM@users.noreply.github.com>
2023-11-23 19:26:27 +0900

    Paper Updating
    Paper Updating

--- Assets/Scripts/OculusVRInteractions.cs
+++ Assets/Scripts/OculusVRInteractions.cs
@@ -5,69 +5,59 @@ using UnityEngine;
 public class OculusVRInteractions : MonoBehaviour
 {
     public GameObject[] uiObject; // 활성화 할 UI 오브젝트
-    public LayerMask interactableLayer; // 상호작용 가능한 레이어 마스크
     public GameObject paper;
 
     public int arraySize;
 
-    private bool isTouching = false; // 터치 중인지 여부
+    public bool isTouching = false; // 터치 중인지 여부
 
     // Update is called once per frame
     void Start()
     {
-        arraySize = 7;
-        uiObject = new GameObject[arraySize];
+
     }
 
     private void CheckTouchInteraction()
     {
         if(OVRInput.Get(OVRInput.Button.PrimaryHandTrigger) || OVRInput.Get(OVRInput.Button.SecondaryHandTrigger))
         {
-            if(!isTouching)
+            if(isTouching==true)
             {
                 if(paper != null)
                 {
                     if(paper.name == "Paper_1")
                     {
-                        isTouching = true;
                         uiObject[0].SetActive(true);
                     }
                     else if(paper.name == "Paper_2")
                     {
-                        isTouching = true;
                         uiObject[1].SetActive(true);
                     }
                     else if (paper.name == "Paper_3")
                     {
-                        isTouching = true;
                         uiObject[2].SetActive(true);
                     }
                     else if (paper.name == "Paper_4")
                     {
-                        isTouching = true;
                         uiObject[3].SetActive(true);
                     }
                     else if (paper.name == "Paper_5")
                     {
-                        isTouching = true;
                         uiObject[4].SetActive(true);
                     }
                     else if (paper.name == "Paper_6")
                     {
-                        isTouching = true;
                         uiObject[5].SetActive(true);
                     }
                     else if (paper.name == "Paper_7")
                     {
-                        isTouching = true;
                         uiObject[6].SetActive(true);
                     }
                 }
             }
         }
-        else
+        else if(isTouching == false)
         {
-            isTouching = false;
             for(int i = 0; i < arraySize; i++)
             {
                 uiObject[i].SetActive(false);
@@ -80,10 +70,13 @@ public class OculusVRInteractions : MonoBehaviour
     {
         if(other.CompareTag("Paper"))
         {
-            for(int i = 0; i < arraySize; i++)
-            {
-                paper = other.gameObject;
-            }
+            paper = other.gameObject;
+            isTouching = true;
         }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        isTouching = false;
     }
 }
--- Assets/Scripts/StoryTrigger.cs
+++ Assets/Scripts/StoryTrigger.cs
@@ -33,55 +33,56 @@ public class StoryTrigger : MonoBehaviour
             if (storyTrigger != null)
             {
                 Debug.Log(storyTrigger.name);
-                if (storyTrigger.name == "StoryTrigger1")
+                if (storyTrigger.name == "Story_Trigger1")
                 {
+                    Debug.Log("test3");
                     storyUi[0].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger2")
+                else if (storyTrigger.name == "Story_Trigger2")
                 {
                     storyUi[1].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger3")
+                else if (storyTrigger.name == "Story_Trigger3")
                 {
                     storyUi[2].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger4")
+                else if (storyTrigger.name == "Story_Trigger4")
                 {
                     storyUi[3].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger5")
+                else if (storyTrigger.name == "Story_Trigger5")
                 {
                     storyUi[4].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger6")
+                else if (storyTrigger.name == "Story_Trigger6")
                 {
                     storyUi[5].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger7")
+                else if (storyTrigger.name == "Story_Trigger7")
                 {
                     storyUi[6].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger8")
+                else if (storyTrigger.name == "Story_Trigger8")
                 {
                     storyUi[7].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger9")
+                else if (storyTrigger.name == "Story_Trigger9")
                 {
                     storyUi[8].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger10")
+                else if (storyTrigger.name == "Story_Trigger10")
                 {
                     storyUi[9].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger11")
+                else if (storyTrigger.name == "Story_Trigger11")
                 {
                     storyUi[10].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger12")
+                else if (storyTrigger.name == "Story_Trigger12")
                 {
                     storyUi[11].SetActive(true);
                 }
-                else if (storyTrigger.name == "StoryTrigger13")
+                else if (storyTrigger.name == "Story_Trigger13")
                 {
                     storyUi[12].SetActive(true);
                 }
@@ -103,6 +104,11 @@ public class StoryTrigger : MonoBehaviour
             storyTrigger = other.gameObject;
             isTrigger = true;
         }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        isTrigger = false;
     }
 
 }
